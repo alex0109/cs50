d4747a6201a66df5618957400c25cb3fae7d30c5
alex0109 <43314783+alex0109@users.noreply.github.com>
2019-02-17 13:32:47 +0200

    tabel.c
    none

---
+++ tabel.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+
+    for(int row = 0; row < 9; row++){
+        for(int column = 0; column < 9; column++){
+            printf("x");
+        }
+        printf("\n");
+    }
+    return 0;
+}
